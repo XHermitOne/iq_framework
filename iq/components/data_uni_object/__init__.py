41e63e4c5fefd93d0b0f63c873e8ba8d38cb8249
xhermit <xhermitone@gmail.com>
2020-04-17 09:51:28 +0700

    Development ...

--- iq/components/data_uni_object/__init__.py
+++ iq/components/data_uni_object/__init__.py
@@ -5,8 +5,8 @@
 Unique data object component package.
 """
 
-# from .spc import SPC
-# from .spc import COMPONENT_TYPE
-# from .component import COMPONENT
+from .spc import SPC
+from .spc import COMPONENT_TYPE
+from .component import COMPONENT
 
 __version__ = (0, 0, 0, 1)
