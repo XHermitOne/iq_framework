1acdb8f8d44a268b93d91166318caf583e4b9987
xhermit <xhermitone@gmail.com>
2020-07-02 15:57:34 +0700

    Проект Sticker. Перенос на платформу IQ

--- iq/util/sys_func.py
+++ iq/util/sys_func.py
@@ -5,9 +5,16 @@
 System functions module.
 """
 
+import sys
 import socket
 import platform
 
+try:
+    # For Python 2
+    import commands as get_procesess_module
+except ImportError:
+    # For Python 3
+    import subprocess as get_procesess_module
 
 __version__ = (0, 0, 0, 1)
 
@@ -38,3 +45,43 @@ def isWindowsPlatform():
 
 def isLinuxPlatform():
     return getPlatform() == 'linux'
+
+
+def getActiveProcessCount(find_process):
+    """
+    The number of active processes running.
+
+    :param find_process: The search string of the process.
+    :return: The number of processes found.
+    """
+    processes_txt = get_procesess_module.getoutput('ps -eo pid,cmd')
+    processes = processes_txt.strip().split('\n')
+    find_processes = [process for process in processes if find_process in process]
+    return len(find_processes)
+
+
+def isActiveProcess(find_process):
+    """
+    Check for the existence of an active executable process.
+
+    :param find_process: The search string of the process.
+    :return: True - process exists / False - process not found.
+    """
+    return getActiveProcessCount(find_process) >= 1
+
+
+def exitForce():
+    """
+    Forced closing of the program.
+    """
+    sys.exit(0)
+
+
+def beep(count=1):
+    """
+    Play system sound.
+
+    :param count: The number of repetitions.
+    """
+    for i in range(count):
+        print('\a')
