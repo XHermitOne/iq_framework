049c7e8460da6d5e53b50b12620d83bda8a6412b
xhermit <xhermitone@gmail.com>
2021-02-19 09:42:36 +0700

    Debuging

--- iq/components/wx_refobjlevelchoicectrl/spc.py
+++ iq/components/wx_refobjlevelchoicectrl/spc.py
@@ -10,7 +10,27 @@ from ...editor import property_editor_id
 
 from ..data_ref_object import spc as data_ref_object_spc
 
+from ...util import global_func
+
 __version__ = (0, 0, 0, 1)
+
+
+def getSortColumns(resource=None, *args, **kwargs):
+    """
+    Get sort column names.
+
+    :param resource: Object resource.
+    :return: Column name list of reference object model.
+    """
+    ref_obj_psp = resource.get('ref_obj', None)
+    if ref_obj_psp:
+        kernel = global_func.getKernel()
+        ref_obj = kernel.createByPsp(psp=ref_obj_psp)
+        model = ref_obj.getModelObj()
+        columns = model.getColumns()
+        return [column.getName() for column in columns]
+    return list()
+
 
 COMPONENT_TYPE = 'iqWxRefObjLevelChoiceCtrl'
 
@@ -46,7 +66,10 @@ WXREFOBJLEVELCHOICECTRL_SPC = {
 
         'label': property_editor_id.STRING_EDITOR,
         'auto_select': property_editor_id.CHECKBOX_EDITOR,
-        'sort_col': property_editor_id.STRING_EDITOR,
+        'sort_col': {
+            'editor': property_editor_id.CHOICE_EDITOR,
+            'choices': getSortColumns,
+        },
         'on_select_code': property_editor_id.EVENT_EDITOR,
     },
     '__help__': {
