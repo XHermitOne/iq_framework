5cd43068f3277e21ee4f35010d6de88b7d457853
xhermit <xhermitone@gmail.com>
2021-04-21 08:31:48 +0700

    Debuging

--- iq/requirements.bat
+++ iq/requirements.bat
@@ -1,38 +1,38 @@
 REM Tested on Windows 10
 
 REM Code analyzers
-C:\Users\user\AppData\Local\Programs\Python\Python38\Scripts\pip3.exe install pylint3
-C:\Users\user\AppData\Local\Programs\Python\Python38\Scripts\pip3.exe install pep8
+C:\Users\user\AppData\Local\Programs\Python\Python38-32\Scripts\pip3.exe install pylint3
+C:\Users\user\AppData\Local\Programs\Python\Python38-32\Scripts\pip3.exe install pep8
 
 REM Operating system
 
 REM Work with the console
-C:\Users\user\AppData\Local\Programs\Python\Python38\Scripts\pip3.exe install colorama
-C:\Users\user\AppData\Local\Programs\Python\Python38\Scripts\pip3.exe install termcolor
+C:\Users\user\AppData\Local\Programs\Python\Python38-32\Scripts\pip3.exe install colorama
+C:\Users\user\AppData\Local\Programs\Python\Python38-32\Scripts\pip3.exe install termcolor
 
 REM wxPython
 
-C:\Users\user\AppData\Local\Programs\Python\Python38\Scripts\pip3.exe install -U wxPython==4.1.1
-C:\Users\user\AppData\Local\Programs\Python\Python38\Scripts\pip3.exe install pysvg
+C:\Users\user\AppData\Local\Programs\Python\Python38-32\Scripts\pip3.exe install -U wxPython==4.1.1
+C:\Users\user\AppData\Local\Programs\Python\Python38-32\Scripts\pip3.exe install pysvg
 
 REM Additionally
-C:\Users\user\AppData\Local\Programs\Python\Python38\Scripts\pip3.exe install objectlistview
-C:\Users\user\AppData\Local\Programs\Python\Python38\Scripts\pip3.exe install matplotlib
+C:\Users\user\AppData\Local\Programs\Python\Python38-32\Scripts\pip3.exe install objectlistview
+C:\Users\user\AppData\Local\Programs\Python\Python38-32\Scripts\pip3.exe install matplotlib
 
 REM JSONRPC 
-C:\Users\user\AppData\Local\Programs\Python\Python38\Scripts\pip3.exe install jsonrpclib-pelix
+C:\Users\user\AppData\Local\Programs\Python\Python38-32\Scripts\pip3.exe install jsonrpclib-pelix
 
 REM Database
-C:\Users\user\AppData\Local\Programs\Python\Python38\Scripts\pip3.exe install psycopg2
-C:\Users\user\AppData\Local\Programs\Python\Python38\Scripts\pip3.exe install sqlalchemy==1.3.24
+C:\Users\user\AppData\Local\Programs\Python\Python38-32\Scripts\pip3.exe install psycopg2
+C:\Users\user\AppData\Local\Programs\Python\Python38-32\Scripts\pip3.exe install sqlalchemy==1.3.24
 
-C:\Users\user\AppData\Local\Programs\Python\Python38\Scripts\pip3.exe install jaydebeapi==1.2.3 jpype1==1.2.1
+C:\Users\user\AppData\Local\Programs\Python\Python38-32\Scripts\pip3.exe install jaydebeapi==1.2.3 jpype1==1.2.1
 
 REM DBase
-C:\Users\user\AppData\Local\Programs\Python\Python38\Scripts\pip3.exe install dbfread
+C:\Users\user\AppData\Local\Programs\Python\Python38-32\Scripts\pip3.exe install dbfread
 
 REM xmltodict
-C:\Users\user\AppData\Local\Programs\Python\Python38\Scripts\pip3.exe install xmltodict
+C:\Users\user\AppData\Local\Programs\Python\Python38-32\Scripts\pip3.exe install xmltodict
 
 
 REM Office
@@ -42,8 +42,9 @@ REM sudo apt install --assume-yes python3-reportlab
 REM sudo apt install --assume-yes python3-pypdf2
 REM sudo apt install --assume-yes python3-odf python-odf-doc
 REM sudo apt install --assume-yes libreoffice-java-common
+C:\Users\user\AppData\Local\Programs\Python\Python38-32\Scripts\pip3.exe install odfpy==1.4.0
 
-C:\Users\user\AppData\Local\Programs\Python\Python38\Scripts\pip3.exe install mtranslate
+C:\Users\user\AppData\Local\Programs\Python\Python38-32\Scripts\pip3.exe install mtranslate
 
 REM sudo apt install --assume-yes python3-jinja2
 
@@ -51,25 +52,19 @@ REM SCADA
 REM sudo apt install --assume-yes gnuplot
 
 REM  Data tables
-C:\Users\user\AppData\Local\Programs\Python\Python38\Scripts\pip3.exe install pandas
+C:\Users\user\AppData\Local\Programs\Python\Python38-32\Scripts\pip3.exe install pandas
 
 REM Maps
-C:\Users\user\AppData\Local\Programs\Python\Python38\Scripts\pip3.exe install folium
-C:\Users\user\AppData\Local\Programs\Python\Python38\Scripts\pip3.exe install yandex-maps
-C:\Users\user\AppData\Local\Programs\Python\Python38\Scripts\pip3.exe install dadata==20.7.0
+C:\Users\user\AppData\Local\Programs\Python\Python38-32\Scripts\pip3.exe install folium
+C:\Users\user\AppData\Local\Programs\Python\Python38-32\Scripts\pip3.exe install yandex-maps
+C:\Users\user\AppData\Local\Programs\Python\Python38-32\Scripts\pip3.exe install dadata==20.7.0
 
 REM Java
-C:\Users\user\AppData\Local\Programs\Python\Python38\Scripts\pip3.exe install py4j
+C:\Users\user\AppData\Local\Programs\Python\Python38-32\Scripts\pip3.exe install py4j
 
 REM iq
 REM export IQ_PATH="$(dirname "$PWD")"
 REM rm ~/.local/lib/python3.5/site-packages/iq.pth
 REM echo $IQ_PATH >> ~/.local/lib/python3.5/site-packages/iq.pth
 
-echo
-echo " _     _____                                 _"
-echo "|_|___|   __|___ ___ _____ ___ _ _ _ ___ ___| |_"
-echo "| | . |   __|  _| .'|     | -_| | | | . |  _| '_|"
-echo "|_|_  |__|  |_| |__,|_|_|_|___|_____|___|_| |_,_|"
-echo "    |_|"
-echo
+echo iqFramework MS Windows
