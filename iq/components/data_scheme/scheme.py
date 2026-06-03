53042c505a675c07bbd5e9bcadbef95291f0e0f1
xhermitone <xhermitone@gmail.com>
2026-06-03 10:27:51 +0700

    Отладка

--- iq/components/data_scheme/scheme.py
+++ iq/components/data_scheme/scheme.py
@@ -106,8 +106,9 @@ class iqSchemeManager(object):
                 return None
 
             engine = db_engine.create(db_url)
-            log_func.debug(str(engine))
-            base.metadata.create_all(engine, checkfirst=True)
+            # log_func.debug(str(engine))
+            # base.metadata.create_all(engine, checkfirst=True)
+            sqlalchemy.MetaData().create_all(bind=engine, checkfirst=True)
 
             # creating a Session class configuration
             session_class = sqlalchemy.orm.sessionmaker(bind=engine, *args, **kwargs)
